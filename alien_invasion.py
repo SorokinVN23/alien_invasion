15698eb72580fc5e183310b197d5b9f9e91f3e75
SorokinVN23 <SorokinVN23@gmail.com>
2025-05-15 20:25:24 +0300

    moving ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,14 +16,15 @@ def run_game():
   screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
   pygame.display.set_caption("Alien Invasion")
   screen.fill(ai_settings.bg_color)
-  ship = Ship(screen)
+  ship = Ship(ai_settings, screen)
 
   # Запуск основного цикла игры.
   while True:
-    gf.check_events()
+    gf.check_events(ship)
     
+    screen.fill(ai_settings.bg_color)
     gf.update_screen(ship)
 
-    time.sleep(0.1)
+    time.sleep(0.001)
 
 run_game()
--- ship.py
+++ ship.py
@@ -4,7 +4,7 @@ import pygame
 
 class Ship():
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """Инициализируем корабль"""
         self.screen = screen
 
@@ -12,12 +12,29 @@ class Ship():
         self.image = pygame.transform.scale(temp_image, (100, 100))
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
-
-        self.rect.centerx = self.screen_rect.centerx
+         
+        self.center = float(self.screen_rect.centerx)
         self.rect.bottom = self.screen_rect.bottom
+
+        self.moving_right = False
+        self.moving_left = False
+
+        self.ai_settings = ai_settings
+
+    def update(self):
+        if self.moving_right:
+            self.center += self.ai_settings.ship_speed_factor
+        elif self.moving_left:
+            self.center -= self.ai_settings.ship_speed_factor  
+
+        if self.center < 0:
+            self.center = 0
+        elif self.center > 1200:
+            self.center = 1200 
 
     def blitme(self):
         """Рисует корабль"""
+        self.rect.centerx = self.center
         self.screen.blit(self.image, self.rect)
 
         
